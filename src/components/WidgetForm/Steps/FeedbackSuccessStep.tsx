02a24638215307a757da9b30b6c64eabc0d57f08
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-05-03 18:48:09 -0300

    feat: add success message

--- src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -1,3 +1,17 @@
+import { CloseButton } from "../../CloseButton";
+import successImage from '../../../assets/success.svg';
+
 export function FeedbackSuccessStep() {
-  return <div>Success</div>
+  return (
+    <>
+      <header>
+        <span className="text-xl leading-6">Success</span>
+        <CloseButton />
+      </header>
+      <div className="flex flex-col items-center py-10 w-[304px]">
+        <img src={successImage} alt="Success Image" />
+        <span className="mt-2 text-xl">We appreciate your feedback!</span>
+      </div>
+    </>
+  )
 }

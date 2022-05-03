35735018c00b9b14a82e6017ea3bd9a42c7f73b1
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-05-03 13:48:02 -0300

    feat: basic structure of widget form

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,7 +1,15 @@
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      Content
+      <header>
+        <span className="text-xl leading-6">Give your feedback</span>
+      </header>
+
+      <p>Content</p>
+
+      <footer className="text-xs text-neutral-400">
+        Made with ♥ by <a href="https://rocketseat.com.br" className="underline underline-offset-2">Rocketseat</a>
+      </footer>
     </div>
   )
 }

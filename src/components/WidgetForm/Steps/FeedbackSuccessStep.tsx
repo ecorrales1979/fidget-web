2d525b68007835dc14c4d64f5e75894246bdff1a
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-05-03 19:02:39 -0300

    feat: add button to send another feedback

--- src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -1,7 +1,11 @@
 import { CloseButton } from "../../CloseButton";
 import successImage from '../../../assets/success.svg';
 
-export function FeedbackSuccessStep() {
+interface Props {
+  onFeedbackRestart: () => void;
+}
+
+export function FeedbackSuccessStep({ onFeedbackRestart }: Props) {
   return (
     <>
       <header>
@@ -11,6 +15,13 @@ export function FeedbackSuccessStep() {
       <div className="flex flex-col items-center py-10 w-[304px]">
         <img src={successImage} alt="Success Image" />
         <span className="mt-2 text-xl">We appreciate your feedback!</span>
+        <button
+          type="button"
+          onClick={onFeedbackRestart}
+          className=" mt-4 py-2 px-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+        >
+          Send another feedback
+        </button>
       </div>
     </>
   )

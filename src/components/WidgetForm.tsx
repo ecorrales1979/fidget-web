cdd3f9d76e0a6bfc42d51292645f55d78d87543b
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-05-03 14:50:09 -0300

    feat: add feedback type selection

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,12 +1,14 @@
-import { CloseButton } from "./CloseButton";
+import { useState } from "react";
 
+import { CloseButton } from "./CloseButton";
 import { FeedbackButton, FeedbackType } from "./FeedbackButton";
 import bugImage from '../assets/bug.svg';
 import ideaImage from '../assets/idea.svg';
 import thoughtImage from '../assets/thought.svg';
 
+type FeedbackKey = "bug" | "idea" | "other";
 
-const feedbackTypes: Record<string, FeedbackType> = {
+const feedbackTypes: Record<FeedbackKey, FeedbackType> = {
   bug: {
     title: 'Problem',
     image: {
@@ -31,6 +33,8 @@ const feedbackTypes: Record<string, FeedbackType> = {
 }
 
 export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackKey | null>(null);
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
@@ -38,11 +42,15 @@ export function WidgetForm() {
         <CloseButton />
       </header>
 
-      <div className="flex py-8 gap-2 w-full">
-        {Object.entries(feedbackTypes).map(([key, value]) => (
-          <FeedbackButton key={key} type={value} />
-        ))}
-      </div>
+      {!feedbackType ? (
+        <div className="flex py-8 gap-2 w-full">
+          {Object.entries(feedbackTypes).map(([key, value]) => (
+            <FeedbackButton key={key} type={value} onClick={() => setFeedbackType(key as FeedbackKey)} />
+          ))}
+        </div>
+      ) : (
+        <p>{feedbackType}</p>
+      )}
 
       <footer className="text-xs text-neutral-400">
         Made with ♥ by <a href="https://rocketseat.com.br" className="underline underline-offset-2">Rocketseat</a>

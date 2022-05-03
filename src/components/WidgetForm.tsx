997ae48ea9bf572d13927b6de481212c12fa351b
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-05-03 14:19:44 -0300

    feat: add category buttons

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,5 +1,41 @@
 import { CloseButton } from "./CloseButton";
 
+import bugImage from '../assets/bug.svg';
+import ideaImage from '../assets/idea.svg';
+import thoughtImage from '../assets/thought.svg';
+
+interface FeedbackType {
+  title: string;
+  image: {
+    source: string;
+    alt: string;
+  }
+}
+
+const feedbackTypes: Record<string, FeedbackType> = {
+  bug: {
+    title: 'Problem',
+    image: {
+      source: bugImage,
+      alt: 'Bug image',
+    }
+  },
+  idea: {
+    title: 'Idea',
+    image: {
+      source: ideaImage,
+      alt: 'Lamp image',
+    }
+  },
+  other: {
+    title: 'Other',
+    image: {
+      source: thoughtImage,
+      alt: 'Cloud image',
+    }
+  },
+}
+
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -8,7 +44,18 @@ export function WidgetForm() {
         <CloseButton />
       </header>
 
-      <p>Content</p>
+      <div className="flex py-8 gap-2 w-full">
+        {Object.entries(feedbackTypes).map(([key, value]) => (
+          <button
+            key={key}
+            type="button"
+            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+          >
+            <img src={value.image.source} alt={value.image.alt} />
+            <span>{value.title}</span>
+          </button>
+        ))}
+      </div>
 
       <footer className="text-xs text-neutral-400">
         Made with ♥ by <a href="https://rocketseat.com.br" className="underline underline-offset-2">Rocketseat</a>
